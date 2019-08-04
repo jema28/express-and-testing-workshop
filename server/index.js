2375e4a049b13c7f7582dc7fab32a935bbeadf32
Jemila <jemila.abulhawa@gmail.com>
2019-08-04 10:39:52 +0300

    shorten server app.listen

--- server/index.js
+++ server/index.js
@@ -3,7 +3,4 @@ because the server is listening still */
 const app = require('./server.js');
 const port = process.env.PORT || 4001;
 
-app.listen(port, () => {
-  console.log(`The things, the things are happening on ${port}!!`);
-  console.log('You can also do this... to show the port %d', port);
-});
+app.listen(port, () => console.log(`Things, the things are happening on ${port}!!`));
