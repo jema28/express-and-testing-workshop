e8426fda5fc4bed5d427860eea5f4417c9b274fd
Jemila <jemila.abulhawa@gmail.com>
2019-08-04 10:38:57 +0300

    replace body parser with express.json

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
-//This is all hopefully familiar by now
 const express = require('express');
-const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const app = express();
 
 // Parses the request payload (if JSON) before the request goes to the handler
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(routes);
 
